94861be7b001af79f80fec3176aaee6adfe9a2bd
louis2jiho <37947547+louis2jiho@users.noreply.github.com>
2019-04-21 00:21:24 +0900

    Add files via upload

---
+++ start.py
@@ -0,0 +1,35 @@
+import asyncio
+import discord
+
+client = discord.Client()
+
+
+
+
+
+@client.event
+async def on_ready():
+    print("Logged in as ")
+    print(client.user.name)
+    print(client.user.id)
+    print("===========")
+
+
+
+
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return None
+
+    id = message.author.id
+    channel = message.channel
+
+    if message.content == '!커맨드':
+        await client.send_message(channel, '커맨드')
+    if message.content == '!커맨드2':
+        await client.send_message(channel, '커맨드2')
+
+
+
+client.run(token)
