9fba637298131fd141b48f7c0bddd3922f589504
louis2jiho <37947547+louis2jiho@users.noreply.github.com>
2019-04-21 10:47:27 +0900

    Update start.py

--- start.py
+++ start.py
@@ -25,10 +25,7 @@ async def on_message(message):
     if message.content == '안녕하세요':
         await client.send_message(channel, '안녕하세요 !명령어 한번씩 쳐주세요')
     if message.content == '!명령어':
-        await client.send_message(channel, '!제작자 - 제작자의 닉네임을 알려 줍니다')
-        await client.send_message(channel, '')
-        await client.send_message(channel, 'NightBot - Music이 있을시')
-        await client.send_message(channel,'!blacklist, !clean, !clear, !disconnect, !id, !joinserver, !listids, !np, !pause, !perms, !play, !pldump, !queue, !restart, !resume, !save, !search, !setavatar, !setname, !setnick, !shuffle, !shutdown, !skip, !stream, !summon, !volume')
+        await client.send_message(channel,'!blacklist, !clean, !clear, !disconnect, !id, !joinserver, !listids, !np, !pause, !perms, !play, !pldump, !queue, !restart, !resume, !save, !search, !setavatar, !setname, !setnick, !shuffle, !shutdown, !skip, !stream, !summon, !volume, !제작자')
     if message.content == '!제작자':
         await client.send_message(channel, '안녕하세요! Jiho Night 입니다')
 
