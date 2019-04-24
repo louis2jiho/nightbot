9916b4e63ddb5c15858fd78795fbd8683a5b86f2
louis2jiho <37947547+louis2jiho@users.noreply.github.com>
2019-04-24 22:41:51 +0900

    Update start.py

--- start.py
+++ start.py
@@ -41,6 +41,18 @@ async def on_message(message):
         await client.send_message(channel,"@ncno1434#4788 이랍니다!!" )
     if message.content == '==모두모여':
         await client.send_message(channel, "@everyone")
+    if message.content.startswith('==')
+        if message.author.server_permissions.administrator or id in owner:
+            learn = message.content.split(' ')
+            mgs = []
+            number = int(learn[1])
+            async for x in client.logs_from(message.channel, limit = number+1):
+                 mgs.append(x)
+            await client.delete_messages(mgs)
+            delembed = discord.Embed(title=메세지 삭제됨",color=0x4286f4)
+            delmsg = await client.send_message(channel,embed = delembed)
+            await asyncio.sleep(5)
+            await client.delete_message(delmsg)
 
 
 access_token = os.environ["BOT_TOKEN"]
